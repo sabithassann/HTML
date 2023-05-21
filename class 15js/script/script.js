09fb378e3b91adcc72353564e78af1c814b72604
sabithassann <sabithassan006@gmail.com>
2023-05-21 13:02:43 +0600

    ADD
    class 15+class 16

---
+++ class 15js/script/script.js
@@ -0,0 +1,3 @@
+let x=50;
+let y=20;
+let result=x+y;
---
+++ class 16.js/testIfStatement.js
@@ -0,0 +1,94 @@
+// let x=10;
+// let y=20;
+// let xy="sabit";
+
+// if(x==y){
+// console.log("True");
+// }
+// else{
+// console.log(xy);
+// }
+// if(x>y){
+//     console.log("False");
+// }
+// else{
+//     console.log("True");
+// }
+// let xyz=0;
+// if(x%3==xyz){
+//     console.log("True");
+// }
+// else{
+//     console.log("False");
+// }
+//
+// 
+// let dhakaWethar=18;
+// if(dhakaWethar>28){
+// console.log("Abnormal Weather")
+// }
+// else if(dhakaWethar>=19){
+// console.log("normal weather")
+// }
+// else if(dhakaWethar>=0){
+//     console.log("cool weather")
+//     }
+
+// else if(dhakaWethar<=0){
+//         console.log("NO");
+
+//     }
+
+// else{
+// console.log("Invalid weather");
+// }
+
+
+
+
+// for(let i=1; i<=50; i++){
+//     console.log(i);
+// }
+// if need to be true 
+
+
+// for(let i=1; i<=20; i++)
+//     if(i%3==0){
+        // console.log("True");
+        // console.log(i);
+
+    // }
+    // else{
+    //     // console.log("False");
+    //     console.log(i);
+
+
+    // }
+
+    // for(let i=1; i<=50; i++)
+    // {
+    //     if(i%3==0 || i%5==0)
+    //     {
+    //         console.log(i);
+
+
+    //     }
+    //     else{
+
+    //     }
+
+    let input=23;
+    let result=1;
+        for(let i=input; i>0; i--){
+            result=result*i;
+            // result*=i;
+            
+        }
+        console.log(result);
+
+
+       
+        
+    
+
+

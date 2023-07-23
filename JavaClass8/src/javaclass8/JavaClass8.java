f9f152a594fb08326cdae3578d3c6a1b96a8f915
sabithassann <sabithassan006@gmail.com>
2023-07-23 12:54:14 +0600

    JAVA

---
+++ JavaClass8/src/javaclass8/JavaClass8.java
@@ -0,0 +1,40 @@
+
+package javaclass8;
+
+import java.util.Scanner;
+import jdk.nashorn.internal.objects.NativeString;
+
+
+public class JavaClass8 {
+
+   
+    public static void main(String[] args) {
+//        Scanner in=new Scanner(System.in);
+//       char ch=in.next().charAt(0);
+//
+////       int ch1=in.nextInt();
+////       int ch1=97;
+////       int ch2=122;
+//       
+//       if(ch>=(char)65 && ch<=(char)90){
+//           System.out.println(Integer.toBinaryString(ch));
+//       }
+//       else if(ch>=(char)97 && ch<=(char)122){
+//           System.out.println(Integer.toBinaryString(ch));
+//           
+//       }
+//       else if(ch>=(char)0 && ch<=(char)64){
+//           System.out.println(Integer.toString(ch));
+//           
+//       }
+//       
+//      else{
+//          System.out.println("RESULT");
+//      }
+
+       String i="HelloJava";
+        System.out.println(i.substring(0,5)+" Jee");  //0 thaka 5 ar age porjonto dekhabe 5 no ta na
+       
+    }
+    
+}

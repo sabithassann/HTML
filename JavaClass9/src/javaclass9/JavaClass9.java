520a8427d6bfbd90dc29a607cb550a665141f27b
sabithassann <sabithassan006@gmail.com>
2023-07-24 13:16:36 +0600

    Class
    ADD

---
+++ JavaClass9/src/javaclass9/JavaClass9.java
@@ -0,0 +1,121 @@
+
+package javaclass9;
+
+import java.util.Scanner;
+
+
+public class JavaClass9 {
+
+
+    public static void main(String[] args) {
+//        for(int i=1;i<=3;i++){
+////            System.out.println("First i "+i);
+//            for(int j=i;j<=i;j++){
+////                System.out.println("Second i "+i);
+//                for(int k=i;k<=j;k++){
+//                    System.out.println("first i "+i+"second i "+j+"Third i "+k);
+//                }
+//                    
+//            }
+//            
+//        }
+
+//       float a=45.260000f;
+//        System.out.printf("count is %f\n",a);
+//        float b=4.5560000000000000000000000000000000000000000000000001f;   //use last sign f for float
+//        System.out.printf("count is %e\n",b);
+//        
+//        String c="Java is cool";
+//        System.out.printf("Really %s\n",c);
+
+
+//        Scanner sc=new Scanner(System.in);
+        
+//        int input=sc.nextInt();
+//        
+//        int sum=0;
+//        while(input !=0){   //0 jodi na hoi totokhon cholo loop ta
+//            
+//            sum+=input;
+//            
+//            System.out.println("Input");
+//            input=sc.nextInt();
+//        }
+//        
+//        System.out.println("The sum is "+ sum);
+
+        
+//        int input1 =sc.nextInt();
+//
+//        
+//        int max=input1;
+//        
+//        if(input1>input2){
+//            System.out.println("Max number is "+input1);
+//        }
+//        else if(input1>input3){
+//            System.out.println("Max number is "+input1);
+//        }
+//        else if(input2>input1){
+//            System.out.println("Max number is "+input2);
+//        }
+//        else if(input2>input3){
+//            System.out.println("Max number is "+input2);
+//        }
+//        else if(input3>input1){
+//            System.out.println("Max number is "+input3);
+//        }
+//        else if(input3>input2)
+//            System.out.println("Max number is "+input3);
+
+
+//           do{
+//               input1=sc.nextInt();
+//               if(input1>max)
+//                   max=input1;
+//           }
+//           while(input1 !=0);
+//
+//         System.out.println("max is "+max);
+//         System.out.println("number is "+input1);
+//         
+//        long input1=sc.nextLong();
+//        long input2=sc.nextLong();
+//        long input3=sc.nextLong();
+//        
+//       
+//        if(input1>=input2 && input1>=input3){
+//            System.out.println("largest number is "+ input1);
+//        }
+//            else if(input2>=input3 && input2>=input1){
+//                 System.out.println("largest number is "+ input2);   
+//           }
+//            else{
+//                System.out.println("largest number is "+ input3);
+//            }
+
+//for(int i=1;i<=10;){
+//    ++i;
+//    System.out.println("HELLO "+i);
+//}
+//        }
+
+
+
+//Tarnari Operator
+//   String a= 9>=10 ? "F":"T";
+//        System.out.println(a);
+
+
+//166 page note homework;
+
+
+int age=16;
+if(age>18)
+    System.out.println("well done");
+        System.out.println("under age");
+
+    }
+    }
+    
+
